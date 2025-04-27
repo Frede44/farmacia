e32de35983df6a5c4261fe83320cdec1f1940b35
Frede44 <falval@miumg.edu.gt>
2025-04-26 21:23:20 -0600

    pantalla login

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view("Auth.Register");
+    }
+
+    public function store(Request $request)
+    {
+        $user =User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+           
+
+
+        auth()->login($user);
+
+        // Logic to create a new user
+
+        return redirect()->route('welcome')->with('success', 'Registration successful!');
+    }
+}
