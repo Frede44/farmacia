9498d3c379f4880be92518e1c0ff98094d92b4e9
Frede44 <falval@miumg.edu.gt>
2025-05-26 09:39:15 -0600

    prueba

--- resources/views/reportes/index.blade.php
+++ resources/views/reportes/index.blade.php
@@ -82,7 +82,13 @@
 
 
 <section class="vista vista1 activa">
-    <div>Contenido de Resumen</div>
+    <div class="grafico_uno">
+        <div id="grafico-ventas" style="height: 370px; width: 100%;"></div>
+    </div>
+    <div class="grafico_dos">
+        <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+    </div>
+   
 </section>
 
 <section class="vista vista2">
@@ -119,6 +125,68 @@
     optionResumen.addEventListener('click', () => mostrarVista('vista1'));
     optionProducto.addEventListener('click', () => mostrarVista('vista2'));
     optionUsuario.addEventListener('click', () => mostrarVista('vista3'));
+</script>
+
+<script>
+window.onload = function () {
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: "Top Oil Reserves"
+	},
+	axisY: {
+		title: "Reserves(MMbbl)"
+	},
+	data: [{        
+		type: "column",  
+		showInLegend: true, 
+		legendMarkerColor: "grey",
+		legendText: "MMbbl = one million barrels",
+		dataPoints: [      
+			{ y: 300878, label: "Venezuela" },
+			{ y: 266455,  label: "Saudi" },
+			{ y: 169709,  label: "Canada" },
+			{ y: 158400,  label: "Iran" },
+			{ y: 142503,  label: "Iraq" },
+			{ y: 101500, label: "Kuwait" },
+			{ y: 97800,  label: "UAE" },
+			{ y: 80000,  label: "Russia" }
+		]
+	}]
+});
+chart.render();
+
+var chart = new CanvasJS.Chart("grafico-ventas", {
+	animationEnabled: true,
+	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: "Top Oil Reserves"
+	},
+	axisY: {
+		title: "Reserves(MMbbl)"
+	},
+	data: [{        
+		type: "column",  
+		showInLegend: true, 
+		legendMarkerColor: "grey",
+		legendText: "MMbbl = one million barrels",
+		dataPoints: [      
+			{ y: 300878, label: "Venezuela" },
+			{ y: 266455,  label: "Saudi" },
+			{ y: 169709,  label: "Canada" },
+			{ y: 158400,  label: "Iran" },
+			{ y: 142503,  label: "Iraq" },
+			{ y: 101500, label: "Kuwait" },
+			{ y: 97800,  label: "UAE" },
+			{ y: 80000,  label: "Russia" }
+		]
+	}]
+});
+chart.render();
+
+}
 </script>
 
 
