9bbe5f135c317495e8eebe2301e4a4545f3e9b82
Frede44 <falval@miumg.edu.gt>
2025-06-04 11:06:35 -0600

    Add favicon link to login and dashboard pages

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Farmacia X</title>
+    <link rel="icon" href="{{ asset('img/LocoFarmacia.png') }}" type="image/png">
     <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
 
     
